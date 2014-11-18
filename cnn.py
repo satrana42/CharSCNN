f275b2bd97b1bec6d8a8b8ec99e100ff47577a88
satwantrana <satwantrana@Satwants-MacBook-Pro.local>
2014-11-18 23:09:31 +0530

    updated

--- cnn.py
+++ cnn.py
@@ -6,11 +6,12 @@ from logistic_sgd import LogisticRegression
 
 class charnn(object):
 	
-	def __init__(self, rng, d, s, k, cl, W=None, C=None, b=None):
+	def __init__(self, rng, d, s, k, cl, word_len, W=None, C=None, b=None):
 		self.d = d
 		self.s = s
 		self.k = k
 		self.cl = cl
+		self.word_len = word_len
 
 		if W == None:
 			self.W = theano.shared(numpy.asarray(rng.uniform(low = -sqrt(6/(d+s)), high = sqrt(6/(d+s)), size=(s,d)),dtype=theano.config.floatX),borrow=True)
@@ -22,7 +23,9 @@ class charnn(object):
 
 		if b == None:
 			self.b  = theano.shared(numpy.asarray(rng.uniform(low = -sqrt(6/(d+s)), high = sqrt(6/(1+cl)), size=(cl)),dtype=theano.config.floatX),borrow=True)
-		else: self.W = W		
+		else: self.W = W	
+
+		self.params = [self.W, self.C, self.b]
 
 	def embed(self, inp):
 		# inp: character matrix of word
@@ -31,7 +34,7 @@ class charnn(object):
 		# ret size: (word_len * s) * (s * d)
 		return T.dot(inp,self.W)
 
-	def conv(self, inp, word_len):
+	def conv(self, inp):
 		# inp: character matrix of word
 		# inp size: word_len * size 
 		# returns: convolutional output
@@ -55,7 +58,7 @@ class charnn(object):
 
 class sentnn(object):
 
-	def __init__(self, rng, cd, cs, ck, cl, wd, ws, wk, wl, cW=None, wW=None, cC=None, wC=None, cb=None, wb=None):
+	def __init__(self, rng, cd, cs, ck, cl, wd, ws, wk, wl, sent_len, cW=None, wW=None, cC=None, wC=None, cb=None, wb=None):
 		
 		self.cd = cd #character embedding dimension
 		self.cs = cs #character vocab size
@@ -66,51 +69,54 @@ class sentnn(object):
 		self.ws = ws
 		self.wk = wk
 		self.wl = wl
-		
+		self.sent_len = sent_len
 		#char embedding matrix
 		if cW == None:
-			self.cW = theano.shared(numpy.asarray(rng.uniform(low = -sqrt(6/(cd+cs)), high = sqrt(6/(cd+cs)), size=(cs,cd)),dtype=theano.config.floatX),borrow=True)
+			self.cW = theano.shared(numpy.asarray(rng.uniform(low = -sqrt(6/(cd+cs)), high = sqrt(6/(cd+cs)), size=(cs,cd)),dtype=theano.config.floatX),name='cW',borrow=True)
 		else: self.cW = cW
 
 		#char conv matrix
 		if cC == None:
-			self.cC = theano.shared(numpy.asarray(rng.uniform(low = -sqrt(6/(cd*ck+cl)), high = sqrt(6/(cd*ck+cl)), size=(cd*ck,cl)),dtype=theano.config.floatX),borrow=True)
+			self.cC = theano.shared(numpy.asarray(rng.uniform(low = -sqrt(6/(cd*ck+cl)), high = sqrt(6/(cd*ck+cl)), size=(cd*ck,cl)),dtype=theano.config.floatX),name='cC',borrow=True)
 		else: self.cC = cC
 
 		#char conv bias
 		if cb == None:
-			self.cb  = theano.shared(numpy.asarray(rng.uniform(low = -sqrt(6/(cl+1)), high = sqrt(6/(cl+1)), size=(cl)),dtype=theano.config.floatX),borrow=True)
+			self.cb  = theano.shared(numpy.asarray(rng.uniform(low = -sqrt(6/(cl+1)), high = sqrt(6/(cl+1)), size=(cl)),dtype=theano.config.floatX),name='cb',borrow=True)
 		else: self.cb = cb	
 
 		if wW == None:
-			self.wW = theano.shared(numpy.asarray(rng.uniform(low = -sqrt(6/(wd+ws)), high = sqrt(6/(wd+ws)), size=(ws,wd)),dtype=theano.config.floatX),borrow=True)
+			self.wW = theano.shared(numpy.asarray(rng.uniform(low = -sqrt(6/(wd+ws)), high = sqrt(6/(wd+ws)), size=(ws,wd)),dtype=theano.config.floatX),name='wW',borrow=True)
 		else: self.wW = wW
 
 		if wC == None:
-			self.wC = theano.shared(numpy.asarray(rng.uniform(low = -sqrt(6/(wd*wk+wl)), high = sqrt(6/(wd*wk+wl)), size=(wd*wk,wl)),dtype=theano.config.floatX),borrow=True)
+			self.wC = theano.shared(numpy.asarray(rng.uniform(low = -sqrt(6/(wd*wk+wl)), high = sqrt(6/(wd*wk+wl)), size=(wd*wk,wl)),dtype=theano.config.floatX),name='wC',borrow=True)
 		else: self.wC = wC
 
 		if wb == None:
-			self.wb  = theano.shared(numpy.asarray(rng.uniform(low = -sqrt(6/(wl+1)), high = sqrt(6/(1+wl)), size=(wl)),dtype=theano.config.floatX),borrow=True)
+			self.wb  = theano.shared(numpy.asarray(rng.uniform(low = -sqrt(6/(wl+1)), high = sqrt(6/(1+wl)), size=(wl)),dtype=theano.config.floatX),name='wb',borrow=True)
 		else: self.wb = wb
 
-		ch = charnn(rng=rng, d=cd, s=cs, k=ck, cl=cl, W=cW, C=cC, b=cb)
-		self.params = [cW, wW, cC, wC, cb, wb]
+		ch = charnn(rng=rng, d=cd, s=cs, k=ck, cl=cl, W=cW, C=cC, b=cb, word_len=0)
+		self.params = [self.cW, self.wW, self.cC, self.wC, self.cb, self.wb]
+		print "sentnn params: ", self.params
 
-	def embed(self, inp, sent_len, sentvec):
+	def embed(self, inp, sentvec):
 		# inp: word matrix of sentence
 		# inp size: sent_len * word_vocab_size 
 		# returns: word + char embedding of sentence
 		# size: sent_len * (cd+wd)
 		wemb = T.dot(inp, self.wW)
 
-		for i in xrange(sent_len.eval()):
-			cemb = ch.conv( w2c(sentvec[i]), wlen(sentvec[i]) )
+		for i in xrange(self.sent_len):
+			ch.word_len =  wlen(sentvec[i]);
+			cemb = ch.conv(w2c(sentvec[i]))
 			numpy.append(wemb[i],cemp)
 
-		return theano.shared(wemb, borrow=True)
+		return wemb
+		#return theano.shared(wemb, borrow=True)
 
-	def conv(self, inp, sent_len, sentvec):
+	def conv(self, inp, sentvec):
 		# inp: word matrix of sent
 		# inp size: sent_len * word_vocab_size 
 		# sent_len: length of sentence
@@ -120,11 +126,11 @@ class sentnn(object):
 		k = self.wk
 		d = self.wd
 		wl = self.wl
-		inp = self.embed(inp,sent_len,sentvec)
+		inp = self.embed(inp,sentvec)
 		inp = numpy.append(numpy.array([[0]*d]*k),inp)
 		inp = numpy.append(inp,numpy.array([[0]*d]*k))
 		conv = numpy.asarray([-100000000]*wl)
-		for i in xrange(sent_len):
+		for i in xrange(self.sent_len):
 			z = inp[k+i-(k/2):k+i+(k-(k/2))]
 			z = T.reshape(z,(k*d))
 			val = T.dot(z,C)+b
@@ -132,32 +138,31 @@ class sentnn(object):
 				conv[i] = max(conv[i],val[i])
 		return theano.shared(conv, borrow=True)
 
-	def eval(self, inp, sent_len, sentvec):
-		return T.nnet.sigmoid(self.conv(inp,sent_len,sentvec))
+	def eval(self, inp, sentvec):
+		return T.nnet.sigmoid(self.conv(inp,sentvec))
 
 def train(char_vocab_size, word_vocab_size, sentvec, inp, labels, learning_rate=0.1, training_epochs=10):
 	#updates neural net with a sentence as input
-	charscnn = sentnn(rng=numpy.random.RandomState(123), cd=5, cs=char_vocab_size, ck=3, cl=50, wd=30, ws=word_vocab_size, wk=5, wl=300)
+	charscnn = sentnn(rng=numpy.random.RandomState(123), cd=5, cs=char_vocab_size, ck=3, cl=50, wd=30, ws=word_vocab_size, wk=5, wl=300, sent_len=0)
 	
 	idx  =T.scalar('idx')
 	x = T.matrix('x')
 	s = T.vector('s')
-	l = T.scalar('l')
-	y = T.scalar('y')
+	y = T.iscalar('y')
 	
-	lr = LogisticRegression(input=charscnn.eval(x, l, s), n_in=300, n_out=1)
+	lr = LogisticRegression(input=charscnn.eval(x, s), n_in=300, n_out=1)
 	
 	# the cost we minimize during training is the NLL of the model
 	cost = lr.negative_log_likelihood(y)
 	
-	params = self.params+lr.params
+	params = charscnn.params+lr.params
+	print params
 	grads = T.grad(cost, params)
-	
 	updates = []
 	for param_i, grad_i in zip(params, grads):
 		updates.append((param_i, param_i - learning_rate * grad_i))
 
-	train_cnn = theano.function([idx], [T.mean(cost)], updates=updates, givens={x:inp[idx], s:sentvec[idx], l:len(sentvec[idx]), y:labels[idx]}, mode="FAST_RUN")
+	train_cnn = theano.function([idx], [T.mean(cost)], updates=updates, givens={x:inp[idx], s:sentvec[idx], y:labels[idx]}, mode="FAST_RUN")
 
 	start_time = time.clock()
 
@@ -170,6 +175,7 @@ def train(char_vocab_size, word_vocab_size, sentvec, inp, labels, learning_rate=
 		# go through trainng set
 		c = []
 		for i in xrange(len(sentvec)):
+			charscnn.sent_len = len(sentvec[i])
 			c.append(train_cnn(i))
 		c_array = numpy.vstack(c)
 		print 'Training epoch %d, reconstruction cost ' % epoch, numpy.mean(c_array[0]), ' jacobian norm ', numpy.mean(numpy.sqrt(c_array[1]))
