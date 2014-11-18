8eb2d1406aacdb0d9d5380d5b16b55b8fbd46746
satwantrana <satwantrana@Satwants-MacBook-Pro.local>
2014-11-18 22:34:49 +0530

    updated

--- cnn.py
+++ cnn.py
@@ -1,10 +1,12 @@
-import numpy, scipy, theano
+import os, time, numpy, scipy, theano
 import theano.tensor as T
 import cPickle as pickle
+from math import *
+from logistic_sgd import LogisticRegression
 
 class charnn(object):
 	
-	def __init__(self, rng, d, s, k, cl, W, C, b):
+	def __init__(self, rng, d, s, k, cl, W=None, C=None, b=None):
 		self.d = d
 		self.s = s
 		self.k = k
@@ -16,11 +18,11 @@ class charnn(object):
 
 		if C == None:
 			self.C = theano.shared(numpy.asarray(rng.uniform(low = -sqrt(6/(d*k+cl)), high = sqrt(6/(d*k+cl)), size=(d*k,cl)),dtype=theano.config.floatX),borrow=True)
-		else self.C = C
+		else: self.C = C
 
 		if b == None:
 			self.b  = theano.shared(numpy.asarray(rng.uniform(low = -sqrt(6/(d+s)), high = sqrt(6/(1+cl)), size=(cl)),dtype=theano.config.floatX),borrow=True)
-		else self.W = W		
+		else: self.W = W		
 
 	def embed(self, inp):
 		# inp: character matrix of word
@@ -53,7 +55,7 @@ class charnn(object):
 
 class sentnn(object):
 
-	def __init__(self, rng, cd, cs, ck, cl, wd, ws, wk, wl, cW, wW, cC, wC, cb, wb):
+	def __init__(self, rng, cd, cs, ck, cl, wd, ws, wk, wl, cW=None, wW=None, cC=None, wC=None, cb=None, wb=None):
 		
 		self.cd = cd #character embedding dimension
 		self.cs = cs #character vocab size
@@ -73,12 +75,12 @@ class sentnn(object):
 		#char conv matrix
 		if cC == None:
 			self.cC = theano.shared(numpy.asarray(rng.uniform(low = -sqrt(6/(cd*ck+cl)), high = sqrt(6/(cd*ck+cl)), size=(cd*ck,cl)),dtype=theano.config.floatX),borrow=True)
-		else self.cC = cC
+		else: self.cC = cC
 
 		#char conv bias
 		if cb == None:
 			self.cb  = theano.shared(numpy.asarray(rng.uniform(low = -sqrt(6/(cl+1)), high = sqrt(6/(cl+1)), size=(cl)),dtype=theano.config.floatX),borrow=True)
-		else self.cb = cb	
+		else: self.cb = cb	
 
 		if wW == None:
 			self.wW = theano.shared(numpy.asarray(rng.uniform(low = -sqrt(6/(wd+ws)), high = sqrt(6/(wd+ws)), size=(ws,wd)),dtype=theano.config.floatX),borrow=True)
@@ -86,11 +88,11 @@ class sentnn(object):
 
 		if wC == None:
 			self.wC = theano.shared(numpy.asarray(rng.uniform(low = -sqrt(6/(wd*wk+wl)), high = sqrt(6/(wd*wk+wl)), size=(wd*wk,wl)),dtype=theano.config.floatX),borrow=True)
-		else self.wC = wC
+		else: self.wC = wC
 
 		if wb == None:
 			self.wb  = theano.shared(numpy.asarray(rng.uniform(low = -sqrt(6/(wl+1)), high = sqrt(6/(1+wl)), size=(wl)),dtype=theano.config.floatX),borrow=True)
-		else self.wb = wb
+		else: self.wb = wb
 
 		ch = charnn(rng=rng, d=cd, s=cs, k=ck, cl=cl, W=cW, C=cC, b=cb)
 		self.params = [cW, wW, cC, wC, cb, wb]
@@ -102,7 +104,7 @@ class sentnn(object):
 		# size: sent_len * (cd+wd)
 		wemb = T.dot(inp, self.wW)
 
-		for i in xrange(sent_len):
+		for i in xrange(sent_len.eval()):
 			cemb = ch.conv( w2c(sentvec[i]), wlen(sentvec[i]) )
 			numpy.append(wemb[i],cemp)
 
@@ -133,7 +135,7 @@ class sentnn(object):
 	def eval(self, inp, sent_len, sentvec):
 		return T.nnet.sigmoid(self.conv(inp,sent_len,sentvec))
 
-def train(learning_rate=0.1, training_epochs=10, char_vocab_size, word_vocab_size, sentvec, inp):
+def train(char_vocab_size, word_vocab_size, sentvec, inp, labels, learning_rate=0.1, training_epochs=10):
 	#updates neural net with a sentence as input
 	charscnn = sentnn(rng=numpy.random.RandomState(123), cd=5, cs=char_vocab_size, ck=3, cl=50, wd=30, ws=word_vocab_size, wk=5, wl=300)
 	
@@ -141,7 +143,7 @@ def train(learning_rate=0.1, training_epochs=10, char_vocab_size, word_vocab_siz
 	x = T.matrix('x')
 	s = T.vector('s')
 	l = T.scalar('l')
-	y = T.vector('y')
+	y = T.scalar('y')
 	
 	lr = LogisticRegression(input=charscnn.eval(x, l, s), n_in=300, n_out=1)
 	
@@ -155,31 +157,35 @@ def train(learning_rate=0.1, training_epochs=10, char_vocab_size, word_vocab_siz
 	for param_i, grad_i in zip(params, grads):
 		updates.append((param_i, param_i - learning_rate * grad_i))
 
-	train_cnn = theano.function([idx], [T.mean(cost)], updates=updates, givens={x:inp[idx], s:sentvec[idx], l:len(sentvec[idx])}, mode="FAST_RUN")
+	train_cnn = theano.function([idx], [T.mean(cost)], updates=updates, givens={x:inp[idx], s:sentvec[idx], l:len(sentvec[idx]), y:labels[idx]}, mode="FAST_RUN")
 
-    start_time = time.clock()
-    
-    ############
-    # TRAINING #
-    ############
+	start_time = time.clock()
 
-    # go through training epochs
-    for epoch in xrange(training_epochs):
-        # go through trainng set
-        c = []
-        for i in xrange(len(sentvec)):
-            c.append(train_cnn(i))
-        c_array = numpy.vstack(c)
-        print 'Training epoch %d, reconstruction cost ' % epoch, numpy.mean(c_array[0]), ' jacobian norm ', numpy.mean(numpy.sqrt(c_array[1]))
-    end_time = time.clock()
+	############
+	# TRAINING #
+	############
 
-    training_time = (end_time - start_time)/60.
+	# go through training epochs
+	for epoch in xrange(training_epochs):
+		# go through trainng set
+		c = []
+		for i in xrange(len(sentvec)):
+			c.append(train_cnn(i))
+		c_array = numpy.vstack(c)
+		print 'Training epoch %d, reconstruction cost ' % epoch, numpy.mean(c_array[0]), ' jacobian norm ', numpy.mean(numpy.sqrt(c_array[1]))
+	end_time = time.clock()
 
-    print "Training Time: ", training_time
+	training_time = (end_time - start_time)/60.
+
+	print "Training Time: ", training_time
 	
 
 #data loading code here
-char_vocab_size, word_vocab_size, sentvec, inp, word_char_mat = pickle.load("data.pkl","rb")
+char_vocab_size, word_vocab_size, sentvec, inp, labels, word_char_mat = pickle.load(open("data.pkl","rb"))
+
+inp = theano.shared(numpy.array(inp))
+labels = theano.shared(numpy.array(labels))
+word_char_mat = theano.shared(numpy.array(word_char_mat))
 
 def w2c(idx):
 	#returns character matrix of a word #idx
@@ -189,7 +195,7 @@ def wlen(idx):
 	#returns len of word #idx
 	return len(word_char_mat[idx])
 
-train(char_vocab_size=char_vocab_size, word_vocab_size=word_vocab_size, sentvec=sentvec, inp=inp)
+train(char_vocab_size=char_vocab_size, word_vocab_size=word_vocab_size, sentvec=sentvec, inp=inp, labels=labels)
 
 
 
